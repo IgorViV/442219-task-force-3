921ff31d0f932048545a4c1d736118ff23bbe4cb
IgorViV <iupgrader@mail.ru>
2022-01-20 15:04:18 +0300

    Adding Yii2 files

---
+++ views/site/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Taskforce';
+\Yii::$app->db->open();
+?>
+
+<div class="center-block">
+    <h1>«TaskForce»</h1>
+    <p>Работа для всех. Найди исполнителя на любую задачу.</p>
+</div>
